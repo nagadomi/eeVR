1f9982caa2e8df7f9fb762dc94dfc6cef19f4790
SAM-tak <osamu.takasugi@gmail.com>
2023-09-11 18:25:19 +0900

    update documentation

--- package.sh
+++ package.sh
@@ -3,5 +3,7 @@ git archive HEAD --output eeVR.tar
 mkdir eeVR
 tar -xvf eeVR.tar -C eeVR
 rm -f eeVR.tar
-zip -FSr eeVR.zip eeVR
+rm -vr eeVR/img
+rm -f eeVR/README.md
+zip -FSr eeVR.zip eeVR -x /img/*
 rm -vr eeVR
